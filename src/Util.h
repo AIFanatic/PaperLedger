e1a670d7761f02b5f43c466af5e9314269385426
NM <nm@powerledger.io>
2019-01-17 00:16:27 +0800

    Fixed bug when in util to handle numbers that are 0. Changed access point name

--- src/Util.h
+++ src/Util.h
@@ -6,10 +6,17 @@
 class Util {
     public:
         static double roundDecimals(double num, int precision) {
+            if(num == 0) {
+                return 0;
+            }
             return roundf(num * pow(10, precision)) / pow(10, precision);
         }
 
         static String numToHuman(double num, int precision) {
+            if(num == 0) {
+                return "0";
+            }
+
             if(num >= 1000000000) {
                 return String(Util::roundDecimals(num / 1000000000, precision)) + " B";
             }
